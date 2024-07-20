64ad8adbd0c32127ff1c7d796d8891927b0d2a2d
tscook17 <tobias.s.cook@gmail.com>
2024-07-19 22:29:23 -0600

    added service classes as well as request, result, and handler classes stubs

---
+++ server/src/main/java/service/result/ResultParent.java
@@ -0,0 +1,13 @@
+package service.result;
+
+public class ResultParent {
+    private String errorMessage = null;
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
