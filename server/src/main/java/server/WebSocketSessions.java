abe9983b7156507c33bcbc44441576d176bddb86
tscook17 <tobias.s.cook@gmail.com>
2024-08-06 16:05:42 -0700

    coded out methods of WebSocketSessions

--- server/src/main/java/server/WebSocketSessions.java
+++ server/src/main/java/server/WebSocketSessions.java
@@ -9,15 +9,20 @@ public class WebSocketSessions {
     private HashMap<Integer, Set<Session>> sessionMap = new HashMap<>();
 
     public void addSessionToGame(int gameID, Session session) {
-
+        Set<Session> sessions = sessionMap.get(gameID);
+        sessions.add(session);
+        sessionMap.put(gameID, sessions);
     }
 
     public void removeSessionFromGame(int gameID, Session session) {
-
+        Set<Session> sessions = sessionMap.get(gameID);
+        sessions.remove(session);
     }
 
     public void removeSession(Session session) {
-
+        for (HashMap.Entry<Integer, Set<Session>> pair : sessionMap.entrySet()) {
+            pair.getValue().remove(session);
+        }
     }
 
     public Set<Session> getSessionsForGame(int gameID) {
